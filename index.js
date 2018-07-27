75e96124f61a3965ec15721361cbcd65457f46f4
Jeff Micklos <jeffmicklos@gmail.com>
2018-07-27 15:19:37 -0700

    add a few more things

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});

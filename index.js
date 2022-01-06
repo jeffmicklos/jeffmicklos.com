0f6598bb33e0104ae5854635381fad5a1db7e484
Jeff Micklos <jeffmicklos@gmail.com>
2022-01-05 17:42:23 -0800

    flip

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 function wwwRedirect(req, res, next) {
-  if (req.headers.host.slice(0, 4) === 'www.') {
-    var newHost = req.headers.host.slice(4);
+  if (req.headers.host.slice(0, 4) !== 'www.') {
+    var newHost = 'www.'; //req.headers.host.slice(4);
     return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
   }
   next();

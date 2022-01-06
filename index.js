d941e91125562f5b108795b756d671d3285d4b1b
Jeff Micklos <jeffmicklos@gmail.com>
2022-01-05 17:38:57 -0800

    okay

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+function wwwRedirect(req, res, next) {
+  if (req.headers.host.slice(0, 4) === 'www.') {
+    var newHost = req.headers.host.slice(4);
+    return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+  }
+  next();
+}
+
+app.set('trust proxy', true);
+app.use(wwwRedirect);
+
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/public'));
@@ -8,10 +19,10 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
